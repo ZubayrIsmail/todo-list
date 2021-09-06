5b05612de147341a81e10904c01cd410dc739016
Zubayr Ismail <zubayr.ismail@Zubayr-Ismail.local>
2021-09-06 10:48:34 +0200

    center align form

--- src/components/ListTodos.vue
+++ src/components/ListTodos.vue
@@ -59,28 +59,30 @@
 
       <b-card-body class="text-center">
         <b-card-title>New Todo Item</b-card-title>
-        <b-form @submit="onSubmit" @reset="onReset" v-if="show" style="max-width: 50rem;" align="center">
-          <b-form-group id="input-group-1" label="New Todo:" label-for="input-1">
-            <b-form-input
-                id="input-1"
-                v-model="form.name"
-                placeholder="Enter todo name"
-                required
-            ></b-form-input>
-          </b-form-group>
+        <div align="center">
+          <b-form @submit="onSubmit" @reset="onReset" v-if="show" style="max-width: 50rem;" align="center">
+            <b-form-group id="input-group-1" label="New Todo:" label-for="input-1" >
+              <b-form-input
+                  id="input-1"
+                  v-model="form.name"
+                  placeholder="Enter todo name"
+                  required
+              ></b-form-input>
+            </b-form-group>
 
-          <b-form-group id="input-group-2" label="Todo Description:" label-for="input-2">
-            <b-form-input
-                id="input-2"
-                v-model="form.description"
-                placeholder="Enter todo description"
-                required
-            ></b-form-input>
-          </b-form-group>
+            <b-form-group id="input-group-2" label="Todo Description:" label-for="input-2">
+              <b-form-input
+                  id="input-2"
+                  v-model="form.description"
+                  placeholder="Enter todo description"
+                  required
+              ></b-form-input>
+            </b-form-group>
 
-          <b-button type="submit" variant="primary">Submit</b-button>
-          <b-button type="reset" variant="danger">Reset</b-button>
-        </b-form>
+            <b-button type="submit" variant="primary">Submit</b-button>
+            <b-button type="reset" variant="danger">Reset</b-button>
+          </b-form>
+        </div>
       </b-card-body>
     </b-card>
   </div>
